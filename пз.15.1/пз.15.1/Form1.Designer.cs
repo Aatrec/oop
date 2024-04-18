9a3928af6f26d4e01ebcb6ad0255d5c8ad3b0463
Aatrec <98344574+Aatrec@users.noreply.github.com>
2024-04-18 13:54:57 +0300

    Add files via upload

---
+++ пз.15.1/пз.15.1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace пз._15._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.TextBox();
+            this.Author = new System.Windows.Forms.TextBox();
+            this.Genre = new System.Windows.Forms.TextBox();
+            this.Year = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зберегти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Додати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Видалити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(157, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Завантажити";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Title
+            // 
+            this.Title.Location = new System.Drawing.Point(60, 202);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(100, 22);
+            this.Title.TabIndex = 5;
+            this.Title.TextChanged += new System.EventHandler(this.Title_TextChanged);
+            // 
+            // Author
+            // 
+            this.Author.Location = new System.Drawing.Point(220, 201);
+            this.Author.Name = "Author";
+            this.Author.Size = new System.Drawing.Size(100, 22);
+            this.Author.TabIndex = 6;
+            // 
+            // Genre
+            // 
+            this.Genre.Location = new System.Drawing.Point(389, 201);
+            this.Genre.Name = "Genre";
+            this.Genre.Size = new System.Drawing.Size(100, 22);
+            this.Genre.TabIndex = 7;
+            // 
+            // Year
+            // 
+            this.Year.Location = new System.Drawing.Point(562, 202);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(100, 22);
+            this.Year.TabIndex = 8;
+            this.Year.TextChanged += new System.EventHandler(this.Year_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.Genre);
+            this.Controls.Add(this.Author);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.TextBox Author;
+        private System.Windows.Forms.TextBox Genre;
+        private System.Windows.Forms.TextBox Year;
+    }
+}
+
---
+++ пз.15.1/пз.15.1/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace пз._15._1
+{
+    public partial class Form1 : Form
+    {
+        private List<Book> books = new List<Book>(); // Список книг
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Налаштування DataGridView
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = books;
+
+            //dataGridView1.Columns.Clear();
+            //dataGridView1.Columns.Add("Title", "Title");
+            //dataGridView1.Columns.Add("Author", "Author");
+            //dataGridView1.Columns.Add("Genre", "Genre");
+            //dataGridView1.Columns.Add("Year", "Year");
+        }
+
+        private void AddBookButton_Click(object sender, EventArgs e)
+        {
+            // Додати нову книгу до списку
+            Book newBook = new Book();
+            // Заповніть властивості книги з TextBox або інших елементів у формі
+            books.Add(newBook);
+            RefreshDataGridView();
+        }
+
+        private void RemoveBookButton_Click(object sender, EventArgs e)
+        {
+            // Видалити обрану книгу зі списку
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Book selectedBook = dataGridView1.SelectedRows[0].DataBoundItem as Book;
+                books.Remove(selectedBook);
+                RefreshDataGridView();
+            }
+        }
+
+        private void RefreshDataGridView()
+        {
+            // Оновити вміст DataGridView
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = books;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Зберегти список книг у текстовий файл
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Book>));
+            using (TextWriter writer = new StreamWriter("books.xml"))
+            {
+                serializer.Serialize(writer, books);
+            }
+            MessageBox.Show("Список книг збережено.");
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            // Завантажити список книг з текстового файлу
+            if (File.Exists("books.xml"))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Book>));
+                using (TextReader reader = new StreamReader("books.xml"))
+                {
+                    books = (List<Book>)serializer.Deserialize(reader);
+                }
+                RefreshDataGridView();
+                MessageBox.Show("Список книг завантажено.");
+            }
+            else
+            {
+                MessageBox.Show("Файл зі списком книг не знайдено.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Додати нову книгу до списку
+            Book newBook = new Book();
+            newBook.Title = Title.Text;
+            newBook.Author = Author.Text;
+            newBook.Genre = Genre.Text;
+            newBook.Year = int.Parse(Year.Text);
+            
+            // Заповніть властивості книги з TextBox або інших елементів у формі
+            books.Add(newBook);
+            RefreshDataGridView();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Видалити обрану книгу зі списку
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Book selectedBook = dataGridView1.SelectedRows[0].DataBoundItem as Book;
+                books.Remove(selectedBook);
+                RefreshDataGridView();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Зберегти список книг у текстовий файл
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Book>));
+            using (TextWriter writer = new StreamWriter("books.xml"))
+            {
+                serializer.Serialize(writer, books);
+            }
+            MessageBox.Show("Список книг збережено.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Завантажити список книг з текстового файлу
+            if (File.Exists("books.xml"))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Book>));
+                using (TextReader reader = new StreamReader("books.xml"))
+                {
+                    books = (List<Book>)serializer.Deserialize(reader);
+                }
+                RefreshDataGridView();
+                MessageBox.Show("Список книг завантажено.");
+            }
+            else
+            {
+                MessageBox.Show("Файл зі списком книг не знайдено.");
+            }
+        }
+
+        private void Title_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Year_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Book
+    {
+        // Властивості книги
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        // Додайте інші властивості, які вам потрібні
+    }
+}
